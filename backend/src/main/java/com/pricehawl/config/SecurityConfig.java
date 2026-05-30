215e6aa3e5388369f8986c6cb07692ed2198af32
23020711 <23020711@vnu.edu.vn>
2026-05-30 18:31:37 +0700

    update: swagger

--- backend/src/main/java/com/pricehawl/config/SecurityConfig.java
+++ backend/src/main/java/com/pricehawl/config/SecurityConfig.java
@@ -55,6 +55,16 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
 
+                        // Swagger UI & OpenAPI
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+
                         // Public GET APIs
                         .requestMatchers(HttpMethod.GET,
                                 "/products/**",
